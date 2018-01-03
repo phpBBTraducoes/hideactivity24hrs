b88a866ae02002d8c782ce0ba40420d15ea66cc3
phpBBTraducoes <eunaumtenhoid@outlook.com>
2018-01-03 01:52:29 -0200

    Atualizado

--- language/pt_br/permissions_ha24hrs.php
+++ language/pt_br/permissions_ha24hrs.php
@@ -2,7 +2,7 @@
 /**
  *
  * Hide 24 hour Activity Stats. An extension for the phpBB Forum Software package.
- * Brazilian Portuguese translation by eunaumtenhoid (c) 2017 [ver 1.0.0] (https://github.com/phpBBTraducoes)
+ * Brazilian Portuguese translation by eunaumtenhoid (c) 2018 [ver 1.0.0] (https://github.com/phpBBTraducoes)
  * @copyright (c) 2017, 3Di, https://github.com/3D-I/
  * @license GNU General Public License, version 2 (GPL-2.0)
  *
